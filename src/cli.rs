1d6ecd06beab9072b41feaf149fd0427d90355ad
jldeen <jedeen@outlook.com>
2021-12-15 01:51:18 -0800

    Use arguments from cli file

--- src/cli.rs
+++ src/cli.rs
@@ -6,6 +6,30 @@ pub fn get_app_cli<'a, 'b>(version: &'b str) -> App<'a, 'b> {
         .author("Jessica Deen <jessica.deen@microsoft.com>")
         .about("Easy CLI to control Elgato Keylight")
         .arg(
+            Arg::with_name("switch")
+                .index(1)
+                .required(true)
+                .short("s")
+                .long("switch")
+                .takes_value(true)
+                .value_name("ON/OFF")
+                .help("Switch value for light status: Accepted values are: on, off.")
+        )
+        .arg(
+            Arg::with_name("brightness")
+                .help("Brightness value for light: Accepted values are: low, medium, high.")
+                .required(true)
+                .index(2)
+                .default_value("20"),
+        )
+        .arg(
+            Arg::with_name("temperature")
+                .help("Temperature value for light: Accepted values are: warm, medium, cool.")
+                .required(true)
+                .index(3)
+                .default_value("213"),
+        )
+        .arg(
             Arg::with_name("ELGATO_IP")
                 .long("elgato-ip-address")
                 .short("ip")
@@ -26,26 +50,5 @@ pub fn get_app_cli<'a, 'b>(version: &'b str) -> App<'a, 'b> {
                 .aliases(&["number_of_lights", "number-of-lights", "number of lights"])
                 .env("NUMBER_OF_LIGHTS")
                 .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("SWITCH")
-                .help("Switch value for light status: Accepted values are: on, off.")
-                .required(true)
-                .index(1)
-                .default_value("off"),
-        )
-        .arg(
-            Arg::with_name("BRIGHTNESS")
-                .help("Brightness value for light: Accepted values are: low, medium, high.")
-                .required(true)
-                .index(2)
-                .default_value("medium"),
-        )
-        .arg(
-            Arg::with_name("TEMPERATURE")
-                .help("Temperature value for light: Accepted values are: warm, medium, cool.")
-                .required(true)
-                .index(3)
-                .default_value("medium"),
         )
 }
